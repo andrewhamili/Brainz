21ce22cef370a0973695822ec050e7925ef6268c
angelodgreat <carions46@gmail.com>
2017-02-17 21:59:58 +0800

    Finished Cisco Bug: When going in the last question the score is not added.

--- app/src/main/java/com/shamwow/brainz/frm_cisco.java
+++ app/src/main/java/com/shamwow/brainz/frm_cisco.java
@@ -2,6 +2,7 @@ package com.shamwow.brainz;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,8 @@ import butterknife.ButterKnife;
 
 public class frm_cisco extends AppCompatActivity {
     databasecontroller myDB;
+    private QuestionLibrary qlib = new QuestionLibrary();
+
     QuestionHandler qmsh;
     StringBuffer cha,chb,chc = new StringBuffer();
     Cursor cursor;
@@ -33,9 +36,11 @@ public class frm_cisco extends AppCompatActivity {
     @BindView(R.id.tv_preview) TextView tv_preview;
     @BindView(R.id.tv_score) TextView tv_score;
 
-    String ansstud,answer;
-    int score =0;
-    int counter =1;
+    String choices;
+    private String answer;
+    private int score =0;
+    private int number =0;
+    String ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,76 +52,128 @@ public class frm_cisco extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
-        set_questions();
+         set_questions();
+         conditions();
 //        get_questions();
-        answerme();
+//        answerme();
+
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                get_answer();
-                counter+=1;
-                set_questions();
+
+                if (number==0){
+                    score=0;
+                } else {
+                    if (number != 5){
+                        if (ref==answer){
+                            score=score+1;
+                            updateScore(score);
+                            set_questions();
+
+
+                        }else if (ref==answer){
+                            score=score+1;
+                            updateScore(score);
+                            set_questions();
+
+                        }else if (ref==answer){
+                            score=score+1;
+                            updateScore(score);
+                            set_questions();
+
+                        } else {
+                            updateScore(score);
+                            set_questions();
+
+                        }
+                    } else {
+
+                        if (number==5){
+
+                            updateScore(score);
+
+                            showMessage("Brainz Inc.","Score: "+score);
+
+                            btn_next.setText("Finish");
+                            btn_next.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    finish();
+                                }
+                            });
+                        }
+
+
+                    }
+                }
+
+
+
+
+
+            }
+
+        });
+
+    }
+
+    public void conditions(){
+
+        rb_a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref = rb_a.getText().toString();
             }
         });
+
+        rb_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref = rb_b.getText().toString();
+            }
+        });
+
+        rb_c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref = rb_c.getText().toString();
+            }
+        });
+
+
 
     }
 
 
     public void set_questions(){
 
+    tv_question.setText(qlib.getListQuestions(number));
+        rb_a.setText(qlib.getChoicea(number));
+        rb_b.setText(qlib.getChoiceb(number));
+        rb_c.setText(qlib.getChoicec(number));
+
+        answer = qlib.getCorrectAnswer(number);
+        number++;
+        rb_a.setChecked(false);
+        rb_b.setChecked(false);
+        rb_c.setChecked(false);
+    }
+
+    public void updateScore(int point){
+        tv_score.setText("Score: "+ score);
+    }
 
 
-        if (counter==1){
-            tv_question.setText("What is cisco?");
-            rb_a.setText("Networking");
-            rb_b.setText("Is a fap");
-            rb_c.setText("Dafaq");
-        }else if (counter==2) {
-            tv_question.setText("Dafaq?");
-            rb_a.setText("a");
-            rb_b.setText("b");
-            rb_c.setText("c");
-        }
-
-
-
-
-
-
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
 
     }
 
-    public void get_answer() {
-        if (counter==5){
 
-            if (counter==1){
-                if (tv_preview.getText().toString() == "Networking"){
-                    score += 1;
-                    String result = Integer.toString(score);
-                    tv_score.setText("Score: "+result);
-                }else{
-                    Toast.makeText(frm_cisco.this, "Wrong Answer", Toast.LENGTH_LONG).show();
-                }
-
-            } else if (counter==2){
-                if(tv_preview.getText().toString()=="a"){
-                    score +=1;
-                    String result = Integer.toString(score);
-                    tv_score.setText("Score: "+result);
-                }else{
-                    Toast.makeText(frm_cisco.this,"Wrong Answer",Toast.LENGTH_LONG).show();
-                }
-            }
-
-        }else{
-
-        }
-
-
-
-
-
-    }
 
 
 
@@ -142,36 +199,36 @@ public class frm_cisco extends AppCompatActivity {
 //        }
 //
 //    }
-
-    public void answerme(){
-
-        rb_a.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                answer = rb_a.getText().toString();
-                tv_preview.setText(answer);
-            }
-        });
-
-        rb_b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer = rb_b.getText().toString();
-                tv_preview.setText(answer);
-            }
-        });
-
-        rb_c.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer= rb_c.getText().toString();
-                tv_preview.setText(answer);
-            }
-        });
-
-
-        }
+//
+//    public void answerme(){
+//
+//        rb_a.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                choices = rb_a.getText().toString();
+//                tv_preview.setText(choices);
+//            }
+//        });
+//
+//        rb_b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                choices = rb_b.getText().toString();
+//                tv_preview.setText(choices);
+//            }
+//        });
+//
+//        rb_c.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                choices= rb_c.getText().toString();
+//                tv_preview.setText(choices);
+//            }
+//        });
+//
+//
+//        }
 
 }
 
