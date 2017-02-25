48dd92864dd01ad29f3f646275278e4aa044fdea
angelodgreat <carions46@gmail.com>
2017-02-25 09:13:47 +0800

    Added Exit prompt

--- app/src/main/java/com/shamwow/brainz/MainActivity.java
+++ app/src/main/java/com/shamwow/brainz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.shamwow.brainz;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -35,12 +37,30 @@ public class MainActivity extends AppCompatActivity {
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
-                System.exit(0);
+                showexitprompt();
             }
         });
 
     }
+
+      public void showexitprompt(){
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if (which==DialogInterface.BUTTON_POSITIVE){
+                        finish();
+                        System.exit(0);
+                    } else {
+
+                    }
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+        }
+
 }
 
 
