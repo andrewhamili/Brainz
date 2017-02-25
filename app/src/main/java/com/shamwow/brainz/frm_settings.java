a9e48be78c18b42431db129a799a85570cf1b83f
angelodgreat <carions46@gmail.com>
2017-02-25 09:07:35 +0800

    Added Clear Data in Settings

--- app/src/main/java/com/shamwow/brainz/frm_settings.java
+++ app/src/main/java/com/shamwow/brainz/frm_settings.java
@@ -1,19 +1,26 @@
 package com.shamwow.brainz;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class frm_settings extends AppCompatActivity {
 
-    Button btn_gotoquestionmanager;
+    @BindView(R.id.btn_questions)  Button btn_gotoquestionmanager;
+    @BindView(R.id.btn_clear_data) Button btn_clear_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frm_settings);
+        ButterKnife.bind(this);
 
         btn_gotoquestionmanager = (Button) findViewById(R.id.btn_questions);
         btn_gotoquestionmanager.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +30,40 @@ public class frm_settings extends AppCompatActivity {
                 startActivity(new Intent(frm_settings.this, QuestionHandler.class));
             }
         });
+
+        btn_clear_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showyesno();
+            }
+        });
+    }
+
+    public void showyesno(){
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which==DialogInterface.BUTTON_POSITIVE){
+                    clearPreferences();
+                } else {
+
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
+    }
+
+    private void clearPreferences() {
+        try {
+            // clearing app data
+            Runtime runtime = Runtime.getRuntime();
+            runtime.exec("pm clear com.shamwow.brainz");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
